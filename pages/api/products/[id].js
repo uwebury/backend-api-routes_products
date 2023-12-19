a5197d1882301a8813be9ea6aa2f35f0e980db3a
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-19 15:31:44 +0100

    Created API endpoint for single listing

---
+++ pages/api/products/[id].js
@@ -0,0 +1,8 @@
+import { getProductById } from "@/services/productServices";
+
+export default function handler(request, response) {
+  const { id } = request.query;
+  const product = getProductById(id);
+
+  response.status(200).json(product);
+}
